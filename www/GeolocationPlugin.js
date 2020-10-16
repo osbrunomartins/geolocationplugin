4a3d3860411e1f7618e58d593856dd297ef22e18
Bruno Martins <bruno.martins@outsystems.com>
2020-10-16 13:43:06 +0100

    Update GeolocationPlugin.js

--- www/GeolocationPlugin.js
+++ www/GeolocationPlugin.js
@@ -1,21 +1,18 @@
-cordova.define("com.outsystems.geolocationplugin.GeolocationPlugin", function(require, exports, module) {
-    var exec = require('cordova/exec');
+var exec = require('cordova/exec');
     
-    exports.coolMethod = function (arg0, success, error) {
-        exec(success, error, 'GeolocationPlugin', 'coolMethod', [arg0]);
-    };
+exports.coolMethod = function (arg0, success, error) {
+    exec(success, error, 'GeolocationPlugin', 'coolMethod', [arg0]);
+};
     
     
-    exports.getLocation = function (success, error) {
-        exec(success, error, 'GeolocationPlugin', 'Get', [""]);
-    };
+exports.getLocation = function (success, error) {
+    exec(success, error, 'GeolocationPlugin', 'Get', [""]);
+};
     
-    exports.startCapture = function (time, minDistance, success, error) {
-        exec(success, error, 'GeolocationPlugin', 'Start', ["wait_between", time, "min_distance", minDistance]);
-    };
+exports.startCapture = function (time, minDistance, success, error) {
+    exec(success, error, 'GeolocationPlugin', 'Start', ["wait_between", time, "min_distance", minDistance]);
+};
     
-    exports.stopCapture = function (success, error) {
-        exec(success, error, 'GeolocationPlugin', 'Stop', [""]);
-    };
-    
-    });
+exports.stopCapture = function (success, error) {
+    exec(success, error, 'GeolocationPlugin', 'Stop', [""]);
+};
